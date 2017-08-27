1fc49758f863e633e308ba2a44cc091db767a9bb
joeyfboone <joey@urgentmail.com>
2017-08-27 10:55:43 -0400

    setting values in js

---
+++ assets/javascript/script.js
@@ -0,0 +1,33 @@
+
+
+var wins = 0;
+var losses = 0;
+var random_target = Math.floor(Math.random() * 101) + 1;
+var diamond_1_value = Math.floor(Math.random() * 19) + 1;
+var diamond_2_value = Math.floor(Math.random() * 19) + 1;
+var diamond_3_value = Math.floor(Math.random() * 19) + 1;
+var diamond_4_value = Math.floor(Math.random() * 19) + 1;
+var total_value = 0;
+
+
+// To continue guessing
+if (total_value < random_target) {
+
+
+    
+} 
+
+// to mark as a loss
+else if
+
+    (total_value > random_target) {
+
+    }
+    
+
+
+// to mark as a win
+
+else if (total_value == random_target) {
+wins = wins++
+}
